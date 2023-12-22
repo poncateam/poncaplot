5a56140f7d3f881d4d1851297500267509e5027f
Nicolas Mellado <nmellado0@gmail.com>
2023-12-22 14:57:19 +0100

    Centralise DrawingPasses storage and creation in DataManager

--- src/dataManager.h
+++ src/dataManager.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <map>
 #include <optional>
 #include <utility> //pair
 #include <vector>
@@ -9,6 +10,8 @@
 #include <nanogui/vector.h>
 
 #include "poncaTypes.h"
+#include "drawingPasses/distanceField.h"
+#include "drawingPasses/poncaFitField.h"
 
 
 #ifndef M_PI
@@ -18,12 +21,17 @@
 #define DEFAULT_POINT_ANGLE M_PI / 2.
 
 
+struct DrawingPass;
+
 /// Structure holding shared data
 struct DataManager {
 public:
 //    using KdTree = Ponca::KdTree<DataPoint>;
     using PointContainer  = std::vector<nanogui::Vector3f>; // stores x,y,normal angle in radians
     using VectorType = typename KdTree::VectorType;
+
+    DataManager();
+    ~DataManager();
 
     /// Read access to point collection
     inline const KdTree& getKdTree() const { return m_tree; }
@@ -59,9 +67,52 @@ public:
     /// \param Number of neighbors (3 means current point and 2 closest points: left and right)
     void computeNormals(int k = 3);
 
+    /// Names of the supported drawing passes
+    static constexpr size_t nbSupportedDrawingPasses = 5;
+    const std::map<const std::string, size_t> supportedDrawingPasses {
+                    {"Distance Field", 0},
+                    {"Plane", 1},
+                    {"Sphere", 2},
+                    {"Oriented Sphere", 3},
+                    {"Unoriented Sphere", 4}
+            };
+
+    DrawingPass* getDrawingPass(const std::string& name);
+
+    /// Build a a drawing pass
+    /// \param index of the pass name in supportedDrawingPasses
+    DrawingPass* getDrawingPass(size_t index);
+
+    template <typename Functor>
+    bool processPass(int index, Functor f) {
+        switch (index) {
+            case 0: //Distance Field
+                f(dynamic_cast<DistanceFieldWithKdTree*>(getDrawingPass(0)));
+                break;
+            case 1: // Plane
+                f(dynamic_cast<PlaneFitField*>(getDrawingPass(1)));
+                break;
+            case 2: // Sphere
+                f(dynamic_cast<SphereFitField*>(getDrawingPass(2)));
+                break;
+            case 3: // Oriented Sphere
+                f(dynamic_cast<OrientedSphereFitField*>(getDrawingPass(3)));
+                break;
+            case 4: // Unoriented Sphere
+                f(dynamic_cast<UnorientedSphereFitField*>(getDrawingPass(4)));
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+
 private:
     PointContainer m_points;
     KdTree m_tree;
     std::function<void()> m_updateFunction {[](){}};
+
+    std::array<DrawingPass*,nbSupportedDrawingPasses> m_drawingPasses;
 };
 
